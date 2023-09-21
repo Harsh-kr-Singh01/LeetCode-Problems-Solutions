1d4eaa3c49f7e39f7a51795e0e34fad7b3622abb
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-09-21 12:45:29 +0530

    Time: 113 ms (13.76%) | Memory: 93.7 MB (8.97%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int l = 0, r = 1, ans = 0;
+        while(l < n && r < n){
+            if(prices[l] < prices[r]){
+                ans = max(ans, prices[r] - prices[l]);
+                r++;
+            }
+            else l = r, r++;
+            // The main thing to understand is the l = r thing.
+            // We need out left to be at lowest value.
+            // So, if we are getting the r value even lower than
+            // our current l, then we shall always update our 
+            // l directly to r.
+        }
+        return ans;
+    }
+};
