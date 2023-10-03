dc10fd5a37fd1965d2de1e18845126924c6aaf0c
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-10-03 07:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (7.10%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        // map<int,int> m;
-        // for(int i = 0; i < nums.size(); i++){
-        //     m[nums[i]]++;
+        // int ans = 0;
+        // for(int i = 0; i < nums.size()-1; i++){
+        //     for(int j = i+1; j < nums.size(); j++){
+        //         if(nums[i] == nums[j]) ans++;
+        //     }
         // }
+        // return ans;
+        int maxi = *max_element(nums.begin(),nums.end()), j = 0;
+        vector<int> freq(maxi+1); // Frequency vector
+        for(int i = 0; i < nums.size(); i++){
+            freq[nums[i]] += 1;
+        }
+        // 1 2 3 1 1 3 5 2 5 1 3 --> nums
+        // 0 4 2 3 0 2 --> Freq array
         int ans = 0;
-        for(int i = 0; i < nums.size()-1; i++){
-            for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] == nums[j]) ans++;
-            }
+        for(int i = 0; i < freq.size(); i++){
+            ans += freq[i] * (freq[i] - 1) / 2;
         }
         return ans;
     }
