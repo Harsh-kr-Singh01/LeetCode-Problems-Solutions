b97571c26f6221e43159fbc2e095919cdf9f322f
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-11-01 21:01:46 +0530

    Time: 1748 ms (49.13%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,8 +1,8 @@
-select A.student_id, A.student_name, B.subject_name, 
+SELECT A.student_id, A.student_name, B.subject_name, 
 case when Ex.count is null then 0
 else Ex.count
 end as attended_exams
-from Students A cross join Subjects B
+from Students AS A cross join Subjects AS B
 left join (
   Select student_id, subject_name, count(*) as count
   from Examinations
@@ -10,4 +10,4 @@ left join (
 ) as Ex
 on B.subject_name = Ex.subject_name and A.student_id = Ex.student_id
 group by A.student_id, A.student_name, B.subject_name
-order by A.student_id, B.subject_name
+order by A.student_id, B.subject_name;
