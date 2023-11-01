0751d7c99c85a4b53c7c058cf93620c2085970e0
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-11-01 14:15:02 +0530

    Time: 2113 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -4,5 +4,5 @@ FROM Logs AS A
 JOIN Logs AS B
 ON A.ID-B.ID = 1 AND A.NUM = B.NUM
 JOIN Logs AS C
-ON A.ID-C.ID = 2 AND B.ID-C.ID = 1 AND A.NUM = C.NUM
+ON A.ID-C.ID = 2 AND A.NUM = C.NUM
 ORDER BY ConsecutiveNums;
