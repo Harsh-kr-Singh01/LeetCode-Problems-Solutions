394a5678ac546150b63012050a97c10b878ea3d6
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-11-02 11:48:45 +0530

    Time: 2689 ms (5.89%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,13 +1,20 @@
-SELECT A.student_id, A.student_name, B.subject_name, 
-case when Ex.count is null then 0
-else Ex.count
-end as attended_exams
-from Students AS A cross join Subjects AS B
-left join (
-  Select student_id, subject_name, count(*) as count
-  from Examinations
-  group by student_id, subject_name
-) as Ex
-on B.subject_name = Ex.subject_name and A.student_id = Ex.student_id
-group by A.student_id, A.student_name, B.subject_name
-order by A.student_id, B.subject_name;
+# SELECT A.student_id, A.student_name, B.subject_name, 
+# case when Ex.count is null then 0
+# else Ex.count
+# end as attended_exams
+# from Students AS A cross join Subjects AS B
+# left join (
+#   Select student_id, subject_name, count(*) as count
+#   from Examinations
+#   group by student_id, subject_name
+# ) as Ex
+# on B.subject_name = Ex.subject_name and A.student_id = Ex.student_id
+# group by A.student_id, A.student_name, B.subject_name
+# order by A.student_id, B.subject_name;
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
