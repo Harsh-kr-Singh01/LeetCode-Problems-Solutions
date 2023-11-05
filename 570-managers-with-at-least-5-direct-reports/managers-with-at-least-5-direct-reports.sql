5a6253384f5c3a2409a87d284c7d51761f8e39f6
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-11-05 13:46:37 +0530

    Time: 689 ms (23.46%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,15 @@
+# SELECT E1.NAME
+# FROM Employee as E1
+# WHERE E1.ID IN (
+#     SELECT E2.managerId
+#     FROM Employee as E2
+#     GROUP BY E2.managerId
+#     HAVING COUNT(E2.managerId) >= 5
+# )
+
 SELECT E1.NAME
 FROM Employee as E1
-WHERE E1.ID IN (
-    SELECT E2.managerId
-    FROM Employee as E2
-    GROUP BY E2.managerId
-    HAVING COUNT(E2.managerId) >= 5
-)
+JOIN Employee as E2
+ON E1.ID = E2.managerId
+GROUP BY E2.managerId
+HAVING COUNT(E2.managerId) >= 5;
