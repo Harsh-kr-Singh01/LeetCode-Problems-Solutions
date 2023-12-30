d2c3d947e8a2d7a787c0ddb70d35cce60d6bf9f1
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2023-12-30 12:15:48 +0530

    Time: 9 ms (25.71%) | Memory: 6.3 MB (72.88%) - LeetSync

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     bool digitCount(std::string num) {
         int n = num.length();
-        std::vector<int> counts(n, 0); // Initialize counts vector with size n and all counts as 0
-        
+        vector<int> counts(n, 0);         
         for (int i = 0; i < n; ++i) {
-            int digit = num[i] - '0'; // Convert character to integer value
+            int digit = num[i] - '0'; 
             if (digit >= n) {
-                return false; // If the digit is out of range, return false
+                return false; 
             }
-            counts[digit]++; // Increment count for the digit at its index
-        }
-        
+            counts[digit]++; 
+        }        
         for (int i = 0; i < n; ++i) {
             if (counts[i] != (num[i] - '0')) {
-                return false; // If count doesn't match the digit at its index, return false
+                return false; 
             }
-        }
-        
-        return true; // All conditions hold true
+        }        
+        return true; 
     }
 };
