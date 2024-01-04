d463b7211ffd1429254fe10bfbe39a9b0640f51a
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2024-01-04 08:48:23 +0530

    Time: 2 ms (45.05%) | Memory: 6.3 MB (88.02%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        int prev0 = 1, prev1 = 2;
+        int ans = 0;
+
+        for(int i = 2; i<n; i++){
+            ans = prev0 + prev1;
+            prev0 = prev1;
+            prev1 = ans;
+        }
+        return prev1;
+    }
+};
