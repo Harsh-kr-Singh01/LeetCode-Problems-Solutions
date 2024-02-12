44a44f6a9d5ea62faa9d6be6d9553c93154b5845
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2024-02-12 18:15:46 +0530

    Time: 12 ms (60.00%) | Memory: 42 MB (20.00%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -6,17 +6,17 @@ public:
         int len = pattern.size() + 1;
         int ans = 0;
         for (int i = 0; i <= nums.size() - len; i++) {
-            bool flag = true;
+            bool turn = true;
             for (int j = i, k = 0; j < i + len - 1; j++, k++) {
                 if (pattern[k] == 1 && (nums[j] >= nums[j + 1])) {
-                    flag = false;
+                    turn = false;
                 } else if (pattern[k] == -1 && (nums[j] <= nums[j + 1])) {
-                    flag = false;
+                    turn = false;
                 } else if (pattern[k] == 0 && (nums[j] > nums[j + 1] || nums[j] < nums[j + 1])) {
-                    flag = false;
+                    turn = false;
                 }
             }
-            if (flag) {
+            if (turn) {
                 ans++;
             }
         }
