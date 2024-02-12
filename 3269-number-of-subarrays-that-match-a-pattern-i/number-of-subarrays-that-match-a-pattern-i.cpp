20c20f03b64c3aea4c4f2ba38457a57d31bbd8c2
Harsh-kr-Singh01 <108606365+Harsh-kr-Singh01@users.noreply.github.com>
2024-02-12 18:14:23 +0530

    Time: 11 ms (80.00%) | Memory: 41.9 MB (20.00%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -1,25 +1,24 @@
+#include <vector>
+
 class Solution {
 public:
-    int countMatchingSubarrays(vector<int>& v, vector<int>& p) {
-        int n=v.size();
-        int m=p.size();
-        int ans=0;
-        for(int i=0;i<n;i++) {
-            int f=0;
-            if(i+m>=n)
-                break;
-            for(int j=i;j+1<=i+m;j++) {
-                if(p[j-i]==-1 && v[j]>v[j+1])
-                    continue;
-                if(p[j-i]==0 && v[j]==v[j+1])
-                    continue;
-                if(p[j-i]==1 && v[j]<v[j+1])
-                    continue;
-                f=1;
-                break;
+    int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
+        int len = pattern.size() + 1;
+        int ans = 0;
+        for (int i = 0; i <= nums.size() - len; i++) {
+            bool flag = true;
+            for (int j = i, k = 0; j < i + len - 1; j++, k++) {
+                if (pattern[k] == 1 && (nums[j] >= nums[j + 1])) {
+                    flag = false;
+                } else if (pattern[k] == -1 && (nums[j] <= nums[j + 1])) {
+                    flag = false;
+                } else if (pattern[k] == 0 && (nums[j] > nums[j + 1] || nums[j] < nums[j + 1])) {
+                    flag = false;
+                }
             }
-            if(f==0)
+            if (flag) {
                 ans++;
+            }
         }
         return ans;
     }
